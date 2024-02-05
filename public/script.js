5158f099d68a28e62800b15c3b4eda20370a9477
J3SS3HVA <jesse.leeuwerink@hva.nl>
2024-02-05 13:40:41 +0100

    ver 0.3
    profile card en node js setup

---
+++ public/script.js
@@ -0,0 +1,22 @@
+console.log('hoi')
+const expandButton = document.querySelector('header button')
+const changepfp = document.querySelector('Img')
+expandButton.addEventListener('click', expand)
+
+function expand () {
+  document.body.classList.toggle('expand')
+}
+
+// sphere follows mouse
+document.addEventListener('mousemove', (e) => {
+
+  const mouseFollow = document.getElementById('mouse-follow');
+  const x = e.clientX - 50; //-50 to center div over mouse
+  const y = e.clientY - 50; 
+  console.log(x);
+   
+  mouseFollow.style.top = `${y}px`;
+  mouseFollow.style.left = `${x}px`;
+})
+
+
